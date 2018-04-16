7d28b5764284a4bd5bd2fa98be0780b5140894a1
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2018-04-16 11:52:35 +0200

    -add tutorial and solution to tasks 1-6

---
+++ 06. ASP .NET MVC/MVC_website_IA/Controllers/SongsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_website_IA.Controllers
+{
+    public class SongsController : Controller
+    {
+        // GET: Songs
+        public ActionResult Index()
+        {
+            return Content("Hello World!");
+        }
+
+
+    }
+}
---
+++ 06. ASP .NET MVC/MVC_website_IA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_website_IA.Startup))]
+namespace MVC_website_IA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
