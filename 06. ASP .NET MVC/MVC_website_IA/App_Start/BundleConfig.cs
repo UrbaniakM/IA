174f1777b349e5ed843e5e1e4ad99101175e29a1
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2018-04-17 10:28:18 +0200

    -fix previous commit (Exception thrown when starting app)

--- 06. ASP .NET MVC/MVC_website_IA/App_Start/BundleConfig.cs
+++ 06. ASP .NET MVC/MVC_website_IA/App_Start/BundleConfig.cs
@@ -12,6 +12,7 @@ namespace MVC_website_IA
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
